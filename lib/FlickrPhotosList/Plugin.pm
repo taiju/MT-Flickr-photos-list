06472f052cee8c7e61a3b6f42220f74449735aa3
taiju <higashi@taiju.info>
2010-07-23 10:16:37 +0900

    bugfix

--- lib/FlickrPhotosList/Plugin.pm
+++ lib/FlickrPhotosList/Plugin.pm
@@ -9,6 +9,41 @@ use MT::ErrorHandler;
 use LWP::Simple;
 use XML::Simple;
 
+sub tag {
+    my ($ctx, $args) = @_;
+    my $plugin = MT->component('FlickrPhotosList');
+
+    my $list_style = defined $args->{list_style} ? $args->{list_style} : $plugin->get_config_value('list_style');
+
+    my $params = {
+        method => 'flickr.photos.search',
+        api_key => $plugin->get_config_value('api_key'),
+        user_id => defined $args->{user_id} ? $args->{user_id} : $plugin->get_config_value('user_id'),
+        extras => defined $args->{extras} ? $args->{extras} : $plugin->get_config_value('extras'),
+        per_page => defined $args->{per_page} ? $args->{per_page} : $plugin->get_config_value('per_page'),
+    };
+
+    my $content = get(_buildRequest('http://api.flickr.com/services/rest', $params));
+    return $ctx->error("Flickrから写真を取得できませんでした。") unless defined $content;
+
+    my $xmlobj = XMLin($content);
+    return $ctx->error("Flickrから写真を取得できませんでした。Flickrで発生したエラーの詳細: $xmlobj->{err}->{msg}") if defined $xmlobj->{err};
+
+    my %elem_photos = %{$xmlobj->{photos}->{photo}};
+    my $url = "http://www.flickr.com/photos/$params->{user_id}/";
+    my @lines = map { "<li><a href=\"$url$_/\"><img src=\"$elem_photos{$_}{$params->{extras}}\" alt=\"$elem_photos{$_}{title}\" /></a></li>" } keys %elem_photos;
+    unshift(@lines, "<$list_style>");
+    push(@lines, "</$list_style>");
+    return join("\n", @lines);
+}
+
+sub _buildRequest {
+    my ($api_url, $params) = @_;
+    return "$api_url?" . join('&', map { "$_=$params->{$_}" } keys %{$params});
+}
+
+
+1;
 =head1 Flickr photos List Plugin
 
 This is Movabletype plugin. This plugin get photos from flickr, and outputs ul or ol element.
@@ -37,43 +72,3 @@ Modifieres that you can use are user_id, per_page, extras, and list_style.
 Please refer to FlickrPhotosList-LISENSE.txt
 
 =cut
-
-sub tag {
-    my ($ctx, $args) = @_;
-    my $plugin = MT->component('FlickrPhotosList');
-
-    my $list_style = defined $args->{list_style} ? $args->{list_style} : $plugin->get_config_value('list_style');
-
-    my $params = {
-        method => 'flickr.photos.search',
-        api_key => $plugin->get_config_value('api_key'),
-        user_id => defined $args->{user_id} ? $args->{user_id} : $plugin->get_config_value('user_id'),
-        extras => defined $args->{extras} ? $args->{extras} : $plugin->get_config_value('extras'),
-        per_page => defined $args->{per_page} ? $args->{per_page} : $plugin->get_config_value('per_page'),
-    };
-
-    my $content = get(_buildRequest('http://api.flickr.com/services/rest', $params));
-    return $ctx->error("Flickrから写真を取得できませんでした。") unless defined $content;
-
-    my $xmlobj = XMLin($content);
-    return $ctx->error("Flickrから写真を取得できませんでした。Flickrで発生したエラーの詳細: $xmlobj->{err}->{msg}") if defined $xmlobj->{err};
-
-    my %elem_photos = %{$xmlobj->{photos}->{photo}};
-    my $url = "http://www.flickr.com/photos/$params->{user_id}/";
-    my @lines = ();
-    for my $id (keys %elem_photos) {
-        my $line  = "<li><a href=\"$url$id/\"><img src=\"$elem_photos{$id}{$params->{extras}}\" alt=\"$elem_photos{$id}{title}\" /></a></li>";
-        push(@lines, $line);
-    }
-    unshift(@lines, "<$list_style>");
-    push(@lines, "</$list_style>");
-    return join("\n", @lines);
-}
-
-sub _buildRequest {
-    my ($api_url, $params) = @_;
-    return "$api_url?" . join('&', map { "$_=$params->{$_}" } keys %{$params});
-}
-
-
-1;
